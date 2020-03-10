03217a600dc55e180a3a189b5e45b97fcf7980c4
Mona Lisa <femkedrawings@gmail.com>
2020-03-10 22:20:49 +0100

    af?
    projecten toegevoegd

--- js/java.js
+++ js/java.js
@@ -10,13 +10,24 @@ var button = document.querySelector("#darkmode");
 var layout = document.querySelector("body");
 var layout2 = document.querySelector("nav");
 var layout3 = document.querySelector("#overmijsection");
+var layout4 = document.querySelector("#projectensection");
 
 function show() {
 	layout.classList.toggle("bright");
     layout2.classList.toggle("bright");
-    layout3.classList.toggle("bright");
 }
 
 button.addEventListener("click", show);
 
+function show2() {
+    layout3.classList.toggle("bright");
+}
+
+button.addEventListener("click", show2);
+
+function show3() {
+    layout4.classList.toggle("bright");
+}
+
+button.addEventListener("click", show3);
 /*localStorage setItem*/
